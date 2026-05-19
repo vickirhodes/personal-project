9eb6c08e024a53b7951acbf0be8926c521686ace
vickibirks <victoria.birks@brighthr.com>
2026-05-19 13:55:16 +0100

    remove wait times

--- tests/ui/brightHR.spec.ts
+++ tests/ui/brightHR.spec.ts
@@ -5,11 +5,15 @@ test('Add an Annual Leave on BrightHR', async ({ page }) => {
 
 // Log in to BrightHR
     const brightHRPage = new BrightHRPage(page);
-    await brightHRPage.page.goto('https://sandbox-app.brighthr.com/dashboard');
+    await brightHRPage.page.goto('https://sandbox-app.brighthr.com/');
     await brightHRPage.emailInput.fill('victoriauk7@getnada.com');
     await brightHRPage.passwordInput.fill('A123456789');
     await brightHRPage.loginButton.click();
-    await page.waitForTimeout(3000);
+        await page.waitForURL(
+            (url) =>
+                url.origin === 'https://sandbox-app.brighthr.com' &&
+                url.pathname === '/dashboard'
+        );
 
 // verify login was successful
     await expect(brightHRPage.companyName).toBeVisible();
@@ -29,7 +33,14 @@ test('Add an Annual Leave on BrightHR', async ({ page }) => {
     await brightHRPage.annualLeaveStartDateInput.fill('13/07/2026');
     await brightHRPage.annualLeaveNotesInput.fill('this is a note');
     await brightHRPage.addAbsenceButton.click();
-    await page.waitForTimeout(3000);
+    await page.waitForURL((url) => {
+        const isCorrectOrigin = url.origin === 'https://sandbox-app.brighthr.com';
+        const isConfirmationPath = /^\/employee\/\d+\/annual-leave\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/confirmation$/i.test(
+            url.pathname,
+        );
+        const isNewLeave = url.searchParams.get('isNew') === 'true';
+        return isCorrectOrigin && isConfirmationPath && isNewLeave;
+    });
 
 // Verify Annual Leave was added successfully
     await expect(brightHRPage.successMessage).toBeVisible();
